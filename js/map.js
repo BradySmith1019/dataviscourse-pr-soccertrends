374b104b8420a8d98c4f102f66e9f13a21f1f51a
BRADY SMITH <u0903295@umail.utah.edu>
2020-11-09 13:24:39 -0700

    Add map - no interactivity yet

---
+++ js/map.js
@@ -0,0 +1,228 @@
+/**
+ * Data structure for the data associated with an individual country.
+ * the CountryData class will be used to keep the data for drawing your map.
+ * You will use the region to assign a class to color the map!
+ */
+class CountryData {
+    /**
+     *
+     * @param type refers to the geoJSON type- countries are considered features
+     * @param properties contains the value mappings for the data
+     * @param geometry contains array of coordinates to draw the country paths
+     * @param region the country region
+     */
+    constructor(type, id, properties, geometry, region) {
+
+        this.type = type;
+        this.id = id;
+        this.properties = properties;
+        this.geometry = geometry;
+        this.region = region;
+    }
+}
+
+/** Class representing the map view. */
+class Map {
+
+    /**
+     * Creates a Map Object
+     *
+     * @param data the full dataset
+     * @param updateCountry a callback function used to notify other parts of the program when the selected
+     * country was updated (clicked)
+     */
+    constructor(data, updateCountry) {
+        this.projection = d3.geoWinkel3().scale(140).translate([365, 225]);
+        this.nameArray = data.population.map(d => d.geo.toUpperCase());
+        this.populationData = data.population;
+        //this.matchesData = data.matches;
+        this.updateCountry = updateCountry;
+    }
+
+    /**
+     * Renders the map
+     * @param world the json data with the shape of all countries and a string for the activeYear
+     */
+    async drawMap(world) {
+        //note that projection is global!
+
+        // ******* TODO: PART I *******
+
+        // Draw the background (country outlines; hint: use #map-chart)
+        // Make sure to add a graticule (gridlines) and an outline to the map
+
+        // Hint: assign an id to each country path to make it easier to select afterwards
+        // we suggest you use the variable in the data element's id field to set the id
+
+        // Make sure and give your paths the appropriate class (see the .css selectors at
+        // the top of the provided html file)
+
+        // You need to match the country with the region. This can be done using .map()
+        // We have provided a class structure for the data called CountryData that you should assign the paramters to in your mapping
+
+        //TODO - your code goes here
+
+        let geoJson = topojson.feature(world, world.objects.countries);
+        console.log(geoJson);
+        let countriesData = this.populationData.map(function(d, i) {
+            let g = geoJson.features.find(element => element.id === d.geo.toUpperCase());
+            let dReg;
+            if (d.region) {
+                dReg = d.region;
+            }
+            else {
+                dReg = "countries";
+            }
+            return new CountryData("Feature", d.geo.toUpperCase(), d, g, dReg);
+        });
+
+        let greenlandGeometry = geoJson.features.find(element => element.id === "GRL");
+        countriesData.push(new CountryData("Feature", "GRL", undefined, greenlandGeometry, "countries"));
+
+        let antarcticaGeometry = geoJson.features.find(element => element.id === "ATA");
+        countriesData.push(new CountryData("Feature", "ATA", undefined, antarcticaGeometry, "countries"));
+
+        let selectMap = d3.select("#map-chart");
+        let svg = selectMap.append("svg").attr("id", "map-chart-svg").attr("class", "map-chart svg");
+        
+        let path = d3.geoPath().projection(this.projection);
+
+        let paths = svg.selectAll("path").data(countriesData);
+
+        paths.join(
+            enter =>
+                enter
+                    .append("path")
+                    .attr("d", function(d) {
+                        return path(d.geometry);
+                    })
+                    .attr("class", "countries")
+                    .attr("id", function(d) {
+                        return d.id;
+                    })
+                    .on("click", d => this.updateCountry(d.id)),
+            update =>
+                update
+                    .attr("d", path)
+                    .attr("class", "boundary")
+                    .attr("id", function(d) {
+                        return d.id;
+                    }),
+            exit => exit.remove()
+        );
+
+        let graticule = d3.geoGraticule();
+        d3.select("#map-chart-svg").append("path")
+        .datum(graticule).attr("class", "graticule")
+        .attr("d", path).attr("fill", "none");
+        let go = graticule.outline();
+        d3.select("#map-chart-svg").append("path")
+        .datum(go).attr("class", "stroke").attr("d", path);
+    }
+
+    /**
+     * Highlights the selected conutry and region on mouse click
+     * @param activeCountry the country ID of the country to be rendered as selected/highlighted
+     */
+    updateHighlightClick(activeCountry) {
+        // ******* TODO: PART 3 *******
+        // Assign selected class to the target country and corresponding region
+        // Hint: If you followed our suggestion of using classes to style
+        // the colors and markers for countries/regions, you can use
+        // d3 selection and .classed to set these classes on here.
+
+        //TODO - your code goes here
+        if (activeCountry !== null) {
+            
+            let svg = document.getElementById("map-chart-svg");
+
+            for (let i = 0; i < svg.children.length; i++) {
+                let theRegion = svg.children[i].getAttribute("class");
+                if (theRegion === "path.asia.selected-country") {
+                    theRegion = "asia";
+                }
+                if (theRegion === "path.europe.selected-country") {
+                    theRegion = "europe";
+                }
+                if (theRegion === "path.americas.selected-country") {
+                    theRegion = "americas";
+                }
+                if (theRegion === "path.africa.selected-country") {
+                    theRegion = "africa";
+                }
+                if (svg.children[i].id === activeCountry) {
+                    if (theRegion === "asia") {
+                        let regionFill = "#2d7aad";
+                        svg.children[i].setAttribute("class", "path." + theRegion + ".selected-country");
+                        svg.children[i].setAttribute("fill", regionFill);
+                    }
+
+                    else if (theRegion === "africa") {
+                        let regionFill = "#cc9a04";
+                        svg.children[i].setAttribute("class", "path." + theRegion + ".selected-country");
+                        svg.children[i].setAttribute("fill", regionFill);
+                    }
+
+                    else if (theRegion === "americas") {
+                        let regionFill = "#aaba18";
+                        svg.children[i].setAttribute("class", "path." + theRegion + ".selected-country");
+                        svg.children[i].setAttribute("fill", regionFill);
+                    }
+
+                    else if (theRegion === "europe") {
+                        let regionFill = "#7c0238";
+                        svg.children[i].setAttribute("class", "path." + theRegion + ".selected-country");
+                        svg.children[i].setAttribute("fill", regionFill);
+                    }
+                }
+                else {
+                    svg.children[i].setAttribute("class", theRegion);
+                }
+            }
+        }
+
+        else {
+            this.clearHighlight();
+        }
+       
+    }
+
+    /**
+     * Clears all highlights
+     */
+    clearHighlight() {
+        // ******* TODO: PART 3 *******
+        // Clear the map of any colors/markers; You can do this with inline styling or by
+        // defining a class style in styles.css
+
+        // Hint: If you followed our suggestion of using classes to style
+        // the colors and markers for hosts/teams/winners, you can use
+        // d3 selection and .classed to set these classes off here.
+
+        //TODO - your code goes here
+
+        let svg = document.getElementById("map-chart-svg");
+
+        for (let i = 0; i < svg.children.length; i++) {
+            let theRegion = svg.children[i].getAttribute("class");
+            if (theRegion === "path.asia.selected-country") {
+                theRegion = "asia";
+            }
+            if (theRegion === "path.europe.selected-country") {
+                theRegion = "europe";
+            }
+            if (theRegion === "path.americas.selected-country") {
+                theRegion = "americas";
+            }
+            if (theRegion === "path.africa.selected-country") {
+                theRegion = "africa";
+            }
+
+            svg.children[i].setAttribute("class", theRegion);
+
+
+        }
+
+    }
+
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,62 @@
 loadData().then(data => {
 
+    this.activeCountry = null;
+    this.activeYear = 2014;
+    let that = this;
+
+    console.log(data);
+    const worldMap = new Map(data, updateCountry);
+
+    function updateCountry(countryID) {
+        this.activeCountry = countryID;
+        worldMap.updateHighlightClick(this.activeCountry);
+    }
+
+    function updateYear(year) {
+
+        //TODO - your code goes here -
+        this.activeYear = year;
+        gapPlot.drawYearBar();
+        gapPlot.updatePlot(this.activeYear);
+        infoBox.updateTextDescription(this.activeCountry, this.activeYear);
+    }
+
+    d3.json('data/world.json').then(mapData => {
+        worldMap.drawMap(mapData, this.activeYear);
+    });
+
+    document.addEventListener("click", function (e) {
+        updateCountry(null);
+    }, true);
 });
+
+async function loadFile(file) {
+    let data = await d3.csv(file).then(d => {
+        let mapped = d.map(g => {
+            for (let key in g) {
+                let numKey = +key;
+                if (numKey) {
+                    g[key] = +g[key];
+                }
+            }
+            return g;
+        });
+        return mapped;
+    });
+    return data;
+}
+
+async function loadData() {
+    let pop = await loadFile('data/pop.csv');
+    let matches = await loadFile('data/WorldCupMatches(1).csv');
+    let players = await loadFile('data/WorldCupPlayers.csv');
+    let cups = await loadFile('data/WorldCups(1).csv');
+
+    return {
+        'population': pop,
+        'matches': matches,
+        'players': players,
+        'cups': cups
+    };
+}
+
